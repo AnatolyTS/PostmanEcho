f64eeacd18e19ca37fbe857dbd13cb0316983acf
Anatoly Tsybrov <ttoljke@gmail.com>
2021-01-15 18:07:18 +0300

    test SUCCESSFUL

--- src/test/java/ru/netology/postma/PostmanEchoTest.java
+++ src/test/java/ru/netology/postma/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some data"))
         ;
     }
 }
